5f327db38811f766140ea79800237e7127031d84
manoj0357-ai <manojmanu035710@gmail.com>
2024-10-11 18:17:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+//import ProductTab from "./ProductTab.jsx";
+//import Button from "./Button.jsx";
+//import Form from "./Form.jsx";
+//import Count from "./Count.jsx";
+import LudoBoard from "./LudoBoard.jsx";
+
+
+function App() {
+  return (
+  <>
+  <p>states in react</p>
+  <LudoBoard />
+  </>
+  );
+}
+export default App;
+
